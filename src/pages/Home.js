e3a523afd61ea44ccfa56443d2aca633280e20ce
신재훈 <sjhh0105@naver.com>
2023-05-04 21:00:42 +0900

    코드리뷰

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,32 +1,33 @@
-import React, { useState } from 'react';
-import InputBox from '../components/InputBox';
-import ToDoItemList from '../components/ToDoItemList';
+import React, { useState } from "react";
+import InputBox from "../components/InputBox";
+import ToDoItemList from "../components/ToDoItemList";
 
 const Home = () => {
-    const [todoList, setTodoList] = useState([]);
+  const [todoList, setTodoList] = useState([]);
 
-    return (
-        <div className="homepage__container">
-            {/* ToDo Item을 추가할 수 있는 input 박스 */}
-            <InputBox todoList={todoList} setTodoList={setTodoList} />
+  return (
+    //
+    <div className="homepage__container">
+      {/* ToDo Item을 추가할 수 있는 input 박스 */}
+      <InputBox todoList={todoList} setTodoList={setTodoList} />
 
-            {/* 할 일 Item 리스트 */}
-            <ToDoItemList
-                title={'할 일'}
-                todoList={todoList}
-                setTodoList={setTodoList}
-                checkedList={false} // (체크되지 않은) 할 일 목록
-            />
+      {/* 할 일 Item 리스트 */}
+      <ToDoItemList
+        title={"할 일"}
+        todoList={todoList}
+        setTodoList={setTodoList}
+        checkedList={false} // (체크되지 않은) 할 일 목록
+      />
 
-            {/* 완료한 Item 리스트 */}
-            <ToDoItemList
-                title={'다 한 일'}
-                todoList={todoList}
-                setTodoList={setTodoList}
-                checkedList={true} // (체크되어 있는)완료한 목록
-            />
-        </div>
-    );
+      {/* 완료한 Item 리스트 */}
+      <ToDoItemList
+        title={"다 한 일"}
+        todoList={todoList}
+        setTodoList={setTodoList}
+        checkedList={true} // (체크되어 있는)완료한 목록
+      />
+    </div>
+  );
 };
 
 export default Home;
