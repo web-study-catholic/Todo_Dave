6a10b147c92a75e70dc15c4e19c5663e309b178c
onejuneee <onejuneee721@gmail.com>
2023-05-11 04:08:47 +0900

    Feat: 리스트 DragList로 수정

--- src/components/ToDoItemList.js
+++ src/components/ToDoItemList.js
@@ -1,3 +1,4 @@
+import DragList from 'react-drag-list';
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import ToDoItem from './ToDoItem';
@@ -13,22 +14,20 @@ const ToDoItemList = ({ title, todoList, setTodoList, checkedList }) => {
     return (
         <div className="todoapp__list">
             <p className="todoapp__list-tit">{title}</p>
-            <ul className="todoapp__list-ul">
-                {todoList &&
-                    todoList.map((todoItem) => {
-                        if (todoItem.deleted) return null;
-                        if (checkedList !== todoItem.checked) return null;
 
-                        return (
-                            <ToDoItem
-                                key={todoItem.id}
-                                todoItem={todoItem}
-                                todoList={todoList}
-                                setTodoList={setTodoList}
-                            />
-                        );
-                    })}
-            </ul>
+            <DragList
+                list={todoList}
+                itemKey="id"
+                template={(todoItem, index) => (
+                    <ToDoItem
+                        key={todoItem.id}
+                        todoItem={todoItem}
+                        todoList={todoList}
+                        setTodoList={setTodoList}
+                    />
+                )}
+                onUpdate={(newList) => setTodoList(newList)}
+            />
         </div>
     );
 };
